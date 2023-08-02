a865fe67a73637db4036fc96070f5dffb6e3c331
Danil650 <danil.id@mail.ru>
2023-08-02 17:36:40 +0500

    first commit

---
+++ BounciBalls/Classes/Ball.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace BounciBalls.Classes
+{
+    internal class Ball
+    {
+        int BallSize = 10;
+        double PositionX = 0;
+        double PositionY = 0;
+        float VelocityX = new Random().Next(-7,7);
+        float VelocityY = new Random().Next(-7, 7);
+        double GridMaxX = 0;
+        double GridMaxY = 0;
+        public Ball()
+        {
+
+        }
+
+        public Ellipse BunceBall { get; set; }
+        public Ball(double gridMaxX, double gridMaxY, MouseButtonEventArgs e, object sender, Grid BallsAreaGrid, Color color, int Size)
+        {
+            BallSize = Size;
+            GridMaxX = gridMaxX;
+            GridMaxY = gridMaxY;
+            Point upPoint = e.GetPosition(sender as Grid);
+            if (upPoint.X + BallSize < 0 || upPoint.X + BallSize > GridMaxX ||
+                    upPoint.Y + BallSize < 0 || upPoint.Y + BallSize > GridMaxY)
+                return;
+            Mouse.Capture(null);
+            Ellipse ellipsNew = new Ellipse();
+            PositionX = upPoint.X;
+            PositionY = upPoint.Y;
+            ellipsNew.Margin = new Thickness(upPoint.X, upPoint.Y, 0, 0);
+            ellipsNew.VerticalAlignment = VerticalAlignment.Top;
+            ellipsNew.HorizontalAlignment = HorizontalAlignment.Left;
+            ellipsNew.Fill = new SolidColorBrush(color);
+            ellipsNew.Width = BallSize;
+            ellipsNew.Height = BallSize;
+            BunceBall = ellipsNew;
+            BallsAreaGrid.Children.Add(ellipsNew);
+        }
+        public void MixColor(Color color)
+        {
+            BunceBall.Fill = new SolidColorBrush(color);
+        }
+        public void MovingBall()
+        {
+            if (PositionX < 0 || PositionX + BallSize > GridMaxX)
+            {
+                VelocityX = -VelocityX;
+            }
+            if (PositionY < 0 || PositionY + BallSize > GridMaxY)
+            {
+                VelocityY = -VelocityY;
+            }
+            PositionX += VelocityX;
+            PositionY += VelocityY;
+            BunceBall.Margin = new Thickness(PositionX, PositionY, 0, 0);
+        }
+    }
+}
---
+++ BounciBalls/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using BounciBalls.Classes;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace BounciBalls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool TimeIsTicking = false;
+        List<Ball> Balls = new List<Ball>();
+        List<Color> Colors = new List<Color>();
+        int MaxSize = 10;
+        public MainWindow()
+        {
+            InitializeComponent();
+  
+            Colors.Add(Color.FromRgb(128, 128, 128));
+            Colors.Add(Color.FromRgb(255, 0, 0));
+            Colors.Add(Color.FromRgb(255, 255, 0));
+        }
+        private DispatcherTimer timer = null;
+        private void timerStart()
+        {
+            if(TimeIsTicking)
+            {
+                timer = new DispatcherTimer();
+                timer.Tick += new EventHandler(timerTick);
+                timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+                timer.Start();
+            }
+            else
+            {
+                timer.Stop();
+                timer = null;
+            }
+
+        }
+        private void timerTick(object sender, EventArgs e)
+        {
+            foreach (Ball ball in Balls)
+            {
+                ball.MovingBall();
+            }
+        }
+        private void BallsAreaGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Ball newBall = new Ball();
+            if (ColorBox.SelectedIndex == 0)
+            {
+                newBall = new Ball(BallsAreaGrid.ActualWidth, BallsAreaGrid.ActualHeight, e, sender, BallsAreaGrid, Colors[new Random().Next(0, ColorBox.Items.Count - 1)], new Random().Next(10, MaxSize));
+
+            }
+            else
+            {
+                newBall = new Ball(BallsAreaGrid.ActualWidth, BallsAreaGrid.ActualHeight, e, sender, BallsAreaGrid, Colors[ColorBox.SelectedIndex - 1], new Random().Next(10, MaxSize));
+
+            }
+            if (newBall.BunceBall != null)
+            {
+                Balls.Add(newBall);
+                CountBallsLbl.Content = $"Колличество шаров на экране: {Balls.Count}";
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MaxSize = (int)MaxSizeSlider.Value;
+        }
+
+        private void CleanAreaBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Balls = new List<Ball>();
+            BallsAreaGrid.Children.Clear();
+            CountBallsLbl.Content = "Колличество шаров на экране: 0";
+
+        }
+
+        private void StartMoveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TimeIsTicking = true;
+
+            timerStart();
+
+            StartMoveBtn.IsEnabled = false;
+
+            StopMoveBtn.IsEnabled = true;
+
+        }
+
+        private void StopMoveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TimeIsTicking = false;
+
+            timerStart();
+
+            StartMoveBtn.IsEnabled = true;
+
+            StopMoveBtn.IsEnabled = false;
+        }
+
+        private void MixColorsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Ball item in Balls)
+            {
+                item.MixColor(Colors[new Random().Next(0, Colors.Count)]);
+            }
+        }
+    }
+}
